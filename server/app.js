27d2976560b7db66406787ae0b25a4c48af6ba87
ParsaJafarian <108084316+Parsilius@users.noreply.github.com>
2023-10-14 18:25:01 -0400

    Add post routes for api server

--- server/app.js
+++ server/app.js
@@ -3,15 +3,39 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 8080;
 const Password = require('./models/Password');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/', async (req, res) => {
-    res.send("Hello Luca Lapenna");
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
-app.post('/api/password', async (req, res) => {
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', async (req, res) => {
+    res.send(await Password.find({}));
+});
+
+app.get('/:id', async (req, res) => {
+    try {
+        const password = await Password.findById(req.params.id);
+        res.send(password);
+    } catch (e) {
+        res.send("Password not found")
+    }
+});
+
+app.post('/', async (req, res) => {
+    req.body.password = await bcrypt.hash(req.body.password, 12);
     try {
         const password = new Password({
             website: req.body.website,
@@ -20,7 +44,7 @@ app.post('/api/password', async (req, res) => {
         await password.save();
         res.send(password);
     } catch (error) {
-        res.status(500).send(error);
+        res.send(error);
     }
 });
 
@@ -28,3 +52,10 @@ app.listen(port, () => {
     console.log("Server is running on port: " + port);
 });
 
+app.all('*', (req, res, next) => {
+    next(new Error('Page not found'));
+});
+
+app.use((err, req, res, next) => {
+    console.log(err.message);
+});
